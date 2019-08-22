e257f02e31243bed29099a5701ee578bd5d347b8
dhl252483@163.com <dhl252483@163.com>
2019-08-22 16:05:50 +0800

    updata

--- updata.bat
+++ updata.bat
@@ -1,6 +1,6 @@
-"C:\Program Files\Git\bin\git.exe" pull
-"C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "updata"
-"C:\Program Files\Git\bin\git.exe" push
-hexo clean
-hexo d -g
+set Path="C:\Program Files\Git\bin\git.exe"
+%Path% pull
+%Path% add .
+%Path% commit -m "updata"
+%Path% push
+
