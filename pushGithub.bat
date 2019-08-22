3f8da394c1946d9932b9a5448e7ffda4781df3a0
dhl252483@163.com <dhl252483@163.com>
2019-08-22 16:39:02 +0800

    updata

--- pushGithub.bat
+++
@@ -1,5 +0,0 @@
-set Git="C:\Program Files\Git\bin\git.exe"
-%Git% pull
-%Git% add .
-%Git% commit -m "updata"
-%Git% push
--- updata.bat
+++ updata.bat
@@ -1,3 +1,7 @@
-call pushGitHub.bat
-hexo clean
-hexo d -g
+set Git="C:\Program Files\Git\bin\git.exe"
+%Git% pull
+%Git% add .
+%Git% commit -m "updata"
+%Git% push
+call hexo clean
+call hexo d -g
